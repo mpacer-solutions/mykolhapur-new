b3271b68ae2614d2431c37099ed7897a840b2edb
shruti <shrutipatil8220@gmail.com>
2020-01-09 15:52:32 +0530

    changes are done in rankala

--- rankala.php
+++ rankala.php
@@ -1,180 +1,133 @@
+
 <?php
+$active_rankla="active";
 include_once 'header.php';
 ?>
-<html>
-  <head>
-    <title>Rankala</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="css/style1.css">
-    <link rel="stylesheet" href="css/stylefor-panhala-fort.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>  
-  </head>
-  <body>
-
-
-    <div><h1 style="color:blue" align="center">WELCOME TO RANKALA</h1></div>
-
+<main class="container-fluid">
+    <h1 class="page-title">Welcome To Rankala</h1>
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-        <li data-target="#myCarousel" data-slide-to="3"></li>
-        <li data-target="#myCarousel" data-slide-to="4"></li>
-        <li data-target="#myCarousel" data-slide-to="5"></li>
-        <li data-target="#myCarousel" data-slide-to="6"></li>
-        <li data-target="#myCarousel" data-slide-to="7"></li>
-        <li data-target="#myCarousel" data-slide-to="8"></li>
-      </ol>
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner">
-        <div class="item active">
-          <img src="images/rankala9.jpg" width="100%" height="100">
-        </div>
-        <div class="item">
-          <img src="images/rankala2.jpg" width="100%" height="100">
-        </div>
-        <div class="item">
-          <img src="images/rankala4.jpg" width="100%" height="100">
-        </div>
-        <div class="item">
-          <img src="images/rankala5.jpg" width="100%" height="100">
-        </div>
-        <div class="item">
-          <img src="images/rankala6.jpg" width="100%" height="100">
-        </div>
-        <div class="item">
-          <img src="images/rankala7.jpg" width="100%" height="100">
-        </div>
-        <div class="item">
-          <img src="images/rankala8.jpg" width="100%" height="100">
-        </div>
-        <div class="item">
-          <img src="images/rankala9.jpg" width="100%" height="100">
-        </div>
-        <div class="item">
-          <img src="images/rankala10.jpg" width="100%" height="100">
-        </div>
-      </div>
-      <!-- Left and right controls -->
-      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#myCarousel" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-        <span class="sr-only">Next</span>
-      </a>
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+            <li data-target="#myCarousel" data-slide-to="4"></li>
+        </ol>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" >
+            <div class="item active" >
+                <img src="images/Rankala/rankala10.jpg" class="img-responsive">
+            </div>
+            <div class="item" >
+                <img src="images/Rankala/rankala9.jpg" class="img-responsive">
+            </div>
+            <div class="item" >
+                <img src="images/Rankala/rankala2.jpg" class="img-responsive">
+            </div>
+            <div class="item" >
+                <img src="images/Rankala/rankala4.jpg" class="img-responsive">
+            </div>
+            <div class="item" >
+                <img src="images/Rankala/rankala6.jpg" class="img-responsive">
+            </div>
+        </div><!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
-  </div>  
-</div>
-</div>
 
-
-
-<div class="row">
-  <div class="col-md-12">
-    <div class="box">
-
-      <div class="row">
-        <div class="col-md-6">
-
-          <h2 style="color:blue">About Rankala</h2>
-          <p><h3>Rankala is one of the best place in Kolhapur..<br>
-            its a biggest lake in kolhapur.<br>
-            Rankala is surrounded by beautiful parks.
+    <div class="row">
+        <div class="col-md-4">
+            <div class="block">
+                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7643.526649388277!2d74.20642907254323!3d16.688722854003235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc1aaa5d3909f97%3A0x12874bdca6a9d134!2sRankala%20Lake!5e0!3m2!1sen!2sin!4v1578467893211!5m2!1sen!2sin" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="block">
+                <h2 class="block-title">About Rankala</h2>
+                <p>Rankala is one of the best place in Kolhapur..
+            its a biggest lake in kolhapur. Rankala is surrounded by beautiful parks.
             Rankala Lake Overview. Rankala Lake was built by the Chhatrapati rulers of Kolhapur. 
             It is believed that the lake is directly connected to a well located in the Panhala Fort. 
             Other attractions present near the lake are the chariot of Lord Shiva and a temple with a 
-            structure of Nandi (bull).<br>
-            The Padpath Udyan that is built close to the Rankala Lake offers various recreational facilities 
-            like lake side walk. Apart from this,the lake is also well-known among the visitors for 
-            different activities like boating and horse riding.<br>
-            Timing, fees and more info-	
-            Other Details<br>
+            structure of Nandi (bull).The Padpath Udyan that is built close to the Rankala Lake offers various recreational facilities 
+            like lake side walk. Apart from this,the lake is also well-known among the visitors for different activities like boating and horse riding.<br>
+            Timing, fees and more info-	Other Details<br>
             Famous for:Lakes<br>
             Entry:free<br>
-            time:4am to 10pm.<br></h3>
-          </p>
+            time:4am to 10pm.<br>
+                </p>
+            </div>
         </div>
-
-
-        <div class="col-md-6">
-          <div class="box">
-            <h3 style="color:blue">Map</h3>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7643.526649388277!2d74.20642907254323!3d16.688722854003235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc1aaa5d3909f97%3A0x12874bdca6a9d134!2sRankala%20Lake!5e0!3m2!1sen!2sin!4v1578467893211!5m2!1sen!2sin" width="800" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
-</div>
 
+    <h2 class="section-title">Rankala is biggest lake in kolhapur</h2>
 
-
-<div class="row">
-  <div class="col-md-12">
-    <div class="box">
-
-
-      <div class="row">
-        <div class="col-md-4">
-
-          <h2 style="color:blue">Hotels Near Rankala</h2>
-           <p>
-          1.Yatri Niwas<br>
-          2.Lake view<br>
-          3.Water Front<br>
-          4.Dominoz<br>
-          5.Hashtag cafe<br>
-          6.Chorge Misal<br>
-          7.Rajmandir ice-cream parlor</p>
-           <img src="images/hotel.jpg" height="300" width="500">
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+               <img src="images/hotel.jpg" alt="...">
+                <div class="caption text-center">
+                    <h3>Hotels</h3>
+                    <p class="content">
+                       1.Yatri Niwas<br>
+                       2.Lake view<br>
+                       3.Water Front<br>
+                       4.Dominoz<br>
+                       5.Chorge Misal<br>
+                    </p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                </div>
+            </div>
         </div>
 
-
-        <div class="col-md-4">
-          <div class="box">
-            <h2 style="color:blue">Shooping Site</h2>
-            <p>1.D-mart<br>
-              2.Cotton-king<br>
-              3.savni sarees
-            </p>
-            <img src="images/dmart2.jpg" class=" img-responsive" height="300">
-          </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+               <img src="images/dmart2.jpg"alt="...">
+                <div class="caption text-center">
+                    <h3>Shopping Here</h3>
+                    <p class="content">
+                        1.D-mart<br>
+                        2.Cotton-king<br>
+                        3.savni sarees<br>
+                        4.Mahadwar Road
+                    </p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                </div>
+            </div>
         </div>
 
-        <div class="col-md-4">
-
-          <h2 style="color:blue">What to see on Rankala :</h2>
-          <p>1.Shalini palace<br>
-            2.boating house<br>
-            3.Aambai Tank<br>
-            4.Fun-fair</p>
-           <img src ="images/rankala5.jpg" class=" img-responsive" height="250">
-           
-          
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src ="images/Rankala/rankala5.jpg" alt="...">
+                <div class="caption text-center">
+                    <h3>What to see on Rankala</h3>
+                    <p class="content">
+                        1.Shalini palace<br>
+                        2.boating house<br>
+                        3.Aambai Tank<br>
+                        4.Fun-fair<br>
+                        5.SandhyaMath
+                    </p>
+                    
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                </div>
+            </div>
         </div>
 
-
-      </div>
     </div>
-  </div>
 
-  <footer>
-    <?php
-    include_once 'footer.php';
-    ?>
-  </footer>
-  <!-- Footer -->
+    <div class="row">
+   
+</main>
 
-
-</body>
-</html>
-
-
+<?php
+  include_once 'footer.php'
+?>
