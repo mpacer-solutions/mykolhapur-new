9fc8fd720966b0b2a1fbd308e594494fde723ddf
hanmant <hanmant@mpacer.com>
2020-01-09 10:12:58 +0530

    Just making header, footer and index file more stylish with minimum markup

--- header.php
+++ header.php
@@ -1,92 +1,33 @@
-
 <html>
-<head>
-    <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>My Kolhapur</title>
-  <style>
-* {box-sizing: border-box;}
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <title>My Kolhapur</title>
+    </head>
+    <body>
 
-body { 
-  margin: 0;
-  font-family: Arial, Helvetica, sans-serif;
-  background-image:url("images/back1.webm")
+        <header class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="index.php"><img src="images/header/logo2.jpg" alt="MyKolhapur" class="img-responsive">
+                    </a>
+                </div>
+                <div id="navbar2" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="active"><a href="index.php">Home</a></li>
+                        <li><a href="mahalaxmi-temple.php">Mahalaxmi-Temple</a></li>
+                        <li><a href="new-palace.php">New-Palace</a></li>
+                        <li><a href="khasbag-maidan.php">Khasbag-Maidan</a></li>
+                        <li><a href="rankala.php">Rankala</a></li>
+                        <li><a href="Panhala-Fort.php">Panhala-Fort</a></li>
+                        <li><a href="jotiba-temple.php">Jotiba-Temple</a></li>
+                        <li><a href="kaneri-math.php">Kaneri-Math</a></li>
+                        <li><a href="kaneri-math.php">Kaneri-Math</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
 
-}
-
-.header {
-  overflow: hidden;
-  background-color:lemonchiffon;
-  padding: 0 0 0.5em 0;
-}
-
-.header a {
-  float: left;
-  color: black;
-  text-align: center;
-  padding: 12px;
-  text-decoration: none;
-  font-size: 18px; 
-  line-height: 25px;
-  border-radius: 4px;
-}
-
-.header a.logo {
-  font-size: 25px;
-  font-weight: bold;
-}
-
-.header a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-.header a.active {
-  background-color: dodgerblue;
-  color: white;
-}
-
-.header-right {
-  float: right;
-  font-size: 25px;
-  font-weight: bold;
-}
-
-@media screen and (max-width: 500px) {
-  .header a {
-    float: none;
-    display: block;
-    text-align: left;
-  }
-  
-  .header-right {
-    float: none;
-  }
-}
-</style>
-</head>
-<body>
-  <div class="header">
-  <a href="#default" class="logo"> <img src="images/header/logo2.jpg" width="400" height="120"> <br><br>
-    <font face="aparajita" color="red" size="7">जगात भारी आम्ही कोल्हापुरी</font>
-  </a><br>
-  <img src="images/bread.jpg" width="1200" height="120" class="d-inline-block align-top" alt="">
-  <div class="header-right">
-    <br>
-    <a class="" href="index.php">Home</a>
-    <a href="mahalaxmi-temple.php">Mahalaxmi-Temple</a>
-    <a href="new-palace.php">New-Palace</a>
-    <a href="khasbag-maidan.php">Khasbag-Maidan</a>
-    <a href="rankala.php">Rankala</a>
-    <a href="Panhala-Fort.php">Panhala-Fort</a>
-    <a href="jotiba-temple.php">Jotiba-Temple</a>
-    <a href="kaneri-math.php">Kaneri-Math</a>
-    <a href="Contacts">Contact</a>
-    <a href="About">About</a>
-    </div>
-</div>
- 
