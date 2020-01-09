ec69da5e736b958a0e7d7f245120419594a3dfa3
unknown <rajnandinimore598.@gmail.com>
2020-01-09 15:44:37 +0530

    making mahalaxmi-temple active

--- mahalaxmi-temple.php
+++ mahalaxmi-temple.php
@@ -1,154 +1,129 @@
+
 <?php
+$active_mahalaxmi="active";
 include_once 'header.php';
 ?>
 
+<main class="container-fluid">
+    <h1 class="page-title">Welcome To Mahalaxmi Temple</h1>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+            <li data-target="#myCarousel" data-slide-to="4"></li>
+        </ol>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" >
+            <div class="item active" >
+                <img src="images/mahalaxmi-images/m2.jpg" class="img-responsive">
+            </div>
+            <div class="item" >
+                <img src="images/mahalaxmi-images/mahalaxmi2.png" class="img-responsive">
+            </div>
+            <div class="item" >
+                <img src="images/mahalaxmi-images/mahalxmi.jpg" class="img-responsive" >
+            </div>
+            <div class="item" >
+                <img src="images/mahalaxmi-images/mahaxmi4.png" class="img-responsive">
+            </div>
+            <div class="item" >
+                <img src="images/mahalaxmi-images/Mahalakshmi_temple,_Kolhapur.jpg" class="img-responsive">
+            </div>
+        </div><!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
 
-
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <title>My Kolhapur</title>
-        <link rel="stylesheet" href="css/stylefor-panhala-fort.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body>
-
-    <center><h1  style="color:blue"> Welcome to Mahalaxmi Temple </h1></center>
-
-
-
-
-    <div class="">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="box2">
-                    <div class="container-fluid">
-
-                        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                            <!-- Indicators -->
-                            <ol class="carousel-indicators">
-                                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                                <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
-                                <li data-target="#myCarousel" data-slide-to="2" class="active"></li>
-                                <li data-target="#myCarousel" data-slide-to="3" class="active"></li>
-                                <li data-target="#myCarousel" data-slide-to="4" class="active"></li>
-                            </ol>
-                            <!-- Wrapper for slides -->
-                            <div class="carousel-inner">
-                                <div class="item active">
-                                    <img src="images/m1.jpg" alt="Chicago" style="width:100%; height:90%;">
-                                </div>
-                                <div class="item">
-                                    <img src="images/m1.jpg" alt="Chicago" style="width:100%; height:90%;">
-                                </div>
-                                <div class="item">
-                                    <img src="images/m1.jpg" alt="Chicago" style="width:100%; height:90%;">
-                                </div>
-                                <div class="item">
-                                    <img src="images/m1.jpg" alt="Chikago" style="width:100%; height:90%;">
-                                </div>
-                                <div class="item">
-                                    <img src="images/m1.jpg" alt="Chicago" style="width:100%; height:90%;">
-                                </div>
-                            </div>
-                            <!-- Left and right controls -->
-                            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                                <span class="glyphicon glyphicon-chevron-left"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                                <span class="glyphicon glyphicon-chevron-right"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </div>
-                    </div>
-
-                </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="block">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3832.4227862284342!2d74.31055611468527!3d16.147133288832773!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc08fc05ca82b7b%3A0x9031bb56d08a5b4d!2sMahalaxmi%20Temple!5e0!3m2!1sen!2sin!4v1578470130995!5m2!1sen!2sin" width="410" height="350" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
             </div>
         </div>
-    </div>
-    <br>
-    <div class="">
-        <div class="row">
-            <div class="col-md-6">
-                <div class="box4">
-                    <h2 style="color:blue">About Mahalaxmi Temple</h2>
-                    <h4>
-                        <p>  The Shri Mahalakshmi Temple of Kolhapur in Maharashtra, India, is one of the Shakti Peethas also called Dakshin Kashi, listed in various puranas of Hinduism. According to these writings, a shakti peetha is a place associated with Shakti, the goddess of power. </p>
-                        The Kolhapur peetha is of special religious significance, being one of the six places where it is believed one can either obtain salvation from desires or have them fulfilled.<p> The temple takes its name from Mahalakshmi, the consort of Vishnu, and it is believed that the divine couple reside in the area.
+        <div class="col-md-8">
+            <div class="block">
+                <h2 class="block-title">About Mahalaxmi Temple</h2>
+                <p>
+                    The Shri Ambabai (mahalaxmi)Temple of Kolhapur in Maharashtra, India, is one of the most famous temples of Hinduism of the state. Kolhapur and the temple is located on the banks of the Panchganga River, it is about 230km south of Pune.Temple of the goddess Ambabai was built during Chalukya reign. Mounted on a stone platform, the murti of the crowned goddess is made of gemstone and weighs about 40 kilograms. The image of Mahalakshmi carved in black stone is 3 feet in height. The Shri Yantra is carved on one of the walls in the temple. A stone lion (the vahana of the goddess), stands behind the statue. The crown contains an image of the Sheshnag, the serpent of Vishnu.
 
-The temple belongs, architecturally, to the Kannada Chalukya empire, and may have been first built circa 700 AD. Mounted on a stone platform, the image of the four armed and crowned goddess is made of gemstone and weighs about 40 kilograms. </p>The image of Mahalakshmi carved in black stone is 3 feet in height. The Shri yantra is carved on one of the walls in the temple. A stone lion, the vahana of the goddess, stands behind the statue.
-<h4>
-    
+                    In her four hands, the deity of Mahalakshmi holds objects of symbolic value. The lower right hand holds a mhalunga (a citrus fruit), in the upper right, a large mace (kaumodaki) the mace of Vishnu with its head touching the ground, in the upper left a shield and in the lower left, a bowl (panpatra). Unlike most Hindu sacred images, which face north or east, the deity faces West (Pashchim). There is a small open window on the western wall, through which the light of the setting sun falls on the face of the image for three days around the 21st of each March and September.
 
+                    There are a number of other shrines in the courtyard to the Navagrahas, Surya, Mahishasuramardini, Vitthal-Rakhmai, Shiva, Vishnu, Tulja Bhavani and others. Some of these images date back to the 11th century, while some are of recent origin. Also located in the courtyard is the temple tank “Manikarnika Kund”, on whose bank is a shrine to Visweshwar Mahadev.
 
-                    
+                    Kirnotsav (lit. festival of Sun Rays) is celebrated in the Mahalakshmi Temple at Kolhapur, when the rays of the sun fall directly on the deity at the time of sunset. It is said that the Sun god pays his homage to Mahalakshmi Ambabai for three days in a year. This will be on the occasion of “Rath Saptami”.
 
-                </div>
-            </div>
-
-            <div class="col-md-6">
-                <div class="box4">
-                    <h2 style="color:blue">Map</h2>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3832.4227862284342!2d74.31055611468527!3d16.147133288832773!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc08fc05ca82b7b%3A0x9031bb56d08a5b4d!2sMahalaxmi%20Temple!5e0!3m2!1sen!2sin!4v1578470130995!5m2!1sen!2sin" width="900" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
-                </div>
+            
+                   </p>
             </div>
         </div>
     </div>
 
-    <div class="">
-        <div class="row">
-            <div class="col-md-4">
-                <div class="box4">
-                    <h2 style="color:blue">Hotels Near Mahalaxmi Temple</h2>
-                    <p> 1.Sayaji Hotel <br>
-                        2.Yatri Nivas<br>
-                    3.Sham Suman Hotel</p>
+    <h2 class="section-title">Why Kolhapur Is More than Just Its Famous Kolhapuri Chappals</h2>
 
-                    <img src="images/hotel.jpeg"  class="img-responsive"  alt="Chicago" style="width:100%; height:70%;">
-
-
-                </div>
-            </div>
-
-            <div class="col-md-4">
-                <div class="box4">
-                    <h2 style="color:blue">Shopping site in Kolhapur</h2>
-                     <p> 1.DYP City Mall <br>
-                        2.Relience Mall<br>
-                    3.D-Mart</p>
-
-                    <img src="images/shop.jpg" class="img-responsive"  alt="Chicago" style="width:100%; height:70%;">
-
-
-                </div></div>
-            <div class="col-md-4">
-                <div class="box4">
-                    <h2 style="color:blue">What to see in Kolhapur</h2>
-                    <p> 1.Mahalaxmi Temple <br>
-                        2.Panhala<br>
-                    3.Rankala Lake</p>
-                    <img src="images/see.png" class="img-responsive" alt="Chicago"  style="width:100%; height:70%;">
-
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="images/hotel.jpeg" alt="...">
+                <div class="caption text-center">
+                    <h3>Hotels</h3>
+                    <p class="content">
+                       1.Sayaji Hotel <br>
+                      2.Yatri Nivas<br>
+                      3.Sham Suman Hotel<br>
+                      4.Hotel Orient Crown<br>
+                      5.Hotel Tourist
+                    </p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
                 </div>
             </div>
         </div>
+
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="images/shop.jpg" alt="...">
+                <div class="caption text-center">
+                    <h3>Shopping Here</h3>
+                    <p class="content">
+                        1.Mahadwar Road<br>
+                       2.Dwarkadas Shamkumar <br>
+                        3.DYP City Mall <br>
+                        4.Relience Mall<br>
+                      5.D-Mart 
+                    </p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="images/see.png" alt="...">
+                <div class="caption text-center">
+                    <h3>What to see in Mahalaxmi Temple </h3>
+                    <p class="content">
+                       1. Bhavani Mandap<br>
+                       2.Firangai Devi Temple<br>
+                       3. Binkhambi Ganesh Mandir<br>
+                       4.Shri Krishnasarasvati Maharaj Samadhi<br>
+                       5.Mahadev Mandir
+                         </p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                </div>
+            </div>
+        </div>
+
     </div>
-    <?php
-    include_once 'footer.php';
-    ?>
+</main>
 
-
-
-</body>
-</html>
+<?php
+  include_once 'footer.php'
+?>
