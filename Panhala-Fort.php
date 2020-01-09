1bf2223c932a35d4119656c1fe5f64e88b2029c5
vidyashri <mandrevidyashri@gmail.com>
2020-01-09 15:55:51 +0530

    adding new images in panhala

--- Panhala-Fort.php
+++ Panhala-Fort.php
@@ -1,132 +1,122 @@
-<?php
-include_once 'header.php';
-?>
-<center><h1 style="color:blue"> Welcome to Panhala Fort </h1></center>
-<div class="">
-  <div class="row">
-    <div class="col-md-12">
-      <div class="box2">
-        <div class="container-fluid">
 
-          <div id="myCarousel" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-              <li data-target="#myCarousel" data-slide-to="1"></li>
-              <li data-target="#myCarousel" data-slide-to="2"></li>
-            </ol>
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner">
-              <div class="item active">
-                <img src="images/1.png" alt="" style="width:100%; height:50%;">
-              </div>
-              <div class="item">
-                <img src="images/2.jpg" alt="" style="width:100%; height:50%;">
-              </div>
-              <div class="item">
-                <img src="images/3.png" alt="" style="width:100%; height:50%;">
-              </div>
-              <div class="item">
-                <img src="images/4.jpeg" alt="" style="width:100%; height:50%;">
-              </div>
-              <div class="item">
-                <img src="images/5.jpg" alt="" style="width:100%; height:50%;">
-              </div>
+<?php
+include_once 'header.php'
+?>
+
+<main class="container-fluid">
+    <h1 class="page-title">Welcome To Panhala</h1>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+            <li data-target="#myCarousel" data-slide-to="4"></li>
+            <li data-target="#myCarousel" data-slide-to="5"></li>
+        </ol>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" >
+            <div class="item active" >
+                <img src="images/1.png" class="img-responsive">
             </div>
-            <!-- Left and right controls -->
-            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-              <span class="glyphicon glyphicon-chevron-left"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#myCarousel" data-slide="next">
-              <span class="glyphicon glyphicon-chevron-right"></span>
-              <span class="sr-only">Next</span>
-            </a>
-          </div>
+            <div class="item" >
+                <img src="images/2.jpg" class="img-responsive">
+            </div>
+            <div class="item" >
+                <img src="images/3.png" class="img-responsive" >
+            </div>
+            <div class="item" >
+                <img src="images/4.jpeg" class="img-responsive">
+            </div>
+            <div class="item" >
+                <img src="images/5.jpg" class="img-responsive">
+            </div>
+        </div><!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+            <div class="block">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15277.245058725903!2d74.1021946!3d16.8109076!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc1a13a0b6e8df9c3!2sPanhala%20Fort!5e0!3m2!1sen!2sin!4v1578478921459!5m2!1sen!2sin" width="500" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="block">
+                <h2 class="block-title">About Panhala</h2>
+                <p>
+                    Panhala fort (also known as Panhalgad, Pahalla and Panalla (literally "the home of serpents")), is located in Panhala, 20 kilometres northwest of Kolhapur in Maharashtra, India. It is strategically located looking over a pass in the Sahyadri mountain range which was a major trade route from Bijapur in the interior of Maharashtra to the coastal areas.[1] Due to its strategic location, it was the centre of several skirmishes in the Deccan involving the Marathas, the Mughals and the British East India Company, the most notable being the Battle of Pavan Khind. Here, the queen regent of Kolhapur City , Tarabai, spent her formative years. Several parts of the fort and the structures within are still intact.
+                    Panahala fort was built between 1178 and 1209 CE, one of 15 forts (others including Bavda, Bhudargad, Satara, and Vishalgad) built by the Shilahara ruler Bhoja II. It is said that aphorism Kahaan Raja Bhoj, kahan Gangu Teli is associated with this fort. A copper plate found in Satara shows that Raja Bhoja held court at Panhala from 1191–1192 CE. About 1209–10, Bhoja Raja was defeated by Singhana (1209–1247), the most powerful of the Devgiri Yadavas, and the fort subsequently passed into the hands of the Yadavas. Apparently it was not well looked after and it passed through several local chiefs. In 1376 inscriptions record the settlement of Nabhapur to the south-east of the fort.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <h2 class="section-title">Panhala Is One Of The Most Important Historical Place</h2>
+
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="images/panhala-images/h2.jpg" alt="...">
+                <div class="caption text-center">
+                    <h3>Hotels:</h3>
+                    <p class="content">
+                        1.Hill Top<br>
+                        2.Dimond Villa<br>
+                        3.Radhanand Holliday<br>
+                        4.Saptrang Recidancy
+                    </p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                </div>
+            </div>
         </div>
 
-      </div>
-    </div>
-  </div>
-</div>
-<br>
-<div class="">
-  <div class="row">
-    <div class="col-md-6">
-      <div class="box4">
-        <h2 style="color:blue">About Panhala...</h2>
-        <p class="p">
-          Panhala fort (also known as Panhalgad, Pahalla and Panalla (literally "the home of serpents")), is located in Panhala, 20 kilometres northwest of Kolhapur in Maharashtra, India. It is strategically located looking over a pass in the Sahyadri mountain range which was a major trade route from Bijapur in the interior of Maharashtra to the coastal areas.[1] Due to its strategic location, it was the centre of several skirmishes in the Deccan involving the Marathas, the Mughals and the British East India Company, the most notable being the Battle of Pavan Khind. Here, the queen regent of Kolhapur City , Tarabai, spent her formative years. Several parts of the fort and the structures within are still intact.
-          Panahala fort was built between 1178 and 1209 CE, one of 15 forts (others including Bavda, Bhudargad, Satara, and Vishalgad) built by the Shilahara ruler Bhoja II. It is said that aphorism Kahaan Raja Bhoj, kahan Gangu Teli is associated with this fort. A copper plate found in Satara shows that Raja Bhoja held court at Panhala from 1191–1192 CE. About 1209–10, Bhoja Raja was defeated by Singhana (1209–1247), the most powerful of the Devgiri Yadavas, and the fort subsequently passed into the hands of the Yadavas. Apparently it was not well looked after and it passed through several local chiefs. In 1376 inscriptions record the settlement of Nabhapur to the south-east of the fort.
-
-
-        </p>
-
-      </div>
-    </div>
-
-    <div class="col-md-6">
-      <div class="box4">
-        <div class="map">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15277.245058725903!2d74.1021946!3d16.8109076!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc1a13a0b6e8df9c3!2sPanhala%20Fort!5e0!3m2!1sen!2sin!4v1578478921459!5m2!1sen!2sin" width="820" height="500" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="images/panhala-images/h4.jpg" alt="...">
+                <div class="caption text-center">
+                    <h3>Shoping Here:</h3>  
+                    <p class="content">
+                        1.Saptrang Recidancy<br>
+                        2.Laxmi Jewellers
+                    </p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                </div>
+            </div>
         </div>
-      </div>
+
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="images/panhala-images/pan2.jpeg" alt="...">
+                <div class="caption text-center">
+                    <h3>What to see on Panhala </h3>
+                    <p class="content">
+
+
+                        1.Sajja koti<br>
+                        2.Teen Darvaja<br>
+                        3.Dhanya kotha<br>
+                        4.Botanical Garden<br>
+                        5.Bajiprabhu stauch
+
+
+                    </p>
+                    <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+                </div>
+            </div>
+        </div>
+
     </div>
-  </div>
-</div>
-
-<div class="">
-  <div class="row">
-    <div class="col-md-4">
-      <div class="box4">
-        <h2 style="color:blue">Hotels Near Panhala</h2>
-          1.Saptrang Recidancy<br><br>
-          2.Hill Top<br><br>
-          3.Dimond Villa<br><br>
-          4.Radhanand Holliday<br><br>
-
-          <img src="images/panhala-images/h2.jpg" alt="" style="width:100%; height:20%;"><br>
-          <br>
-          <img src="images/panhala-images/h3.jpg" alt="" style="width:100%; height:20%;">
 
 
-      </div>
-    </div>
-
-    <div class="col-md-4">
-      <div class="box4">
-        <h2 style="color:blue">Shopping Site</h2>
-          1.Teen Darvaja<br><br>
-          2.Sajja koti<br><br>
-          3.Bajiprabhu stauch<br><br>
-          4.Dhanya kotha<br><br>
-
-          <img src="images/panhala-images/shop.jpg" alt="" style="width:100%; height:20%;"><br>
-          <br>
-          <img src="images/panhala-images/h4.jpg" alt="" style="width:100%; height:20%;">
-        
-
-      </div></div>
-    <div class="col-md-4">
-      <div class="box4">
-        <h2 style="color:blue">What to on see place</h2>
-          1.Teen Darvaja<br><br>
-          2.Sajja koti<br><br>
-          3.Bajiprabhu stauch<br><br>
-          4.Dhanya kotha<br><br>
-          
-        
-          <img src="images/panhala-images/pan1.jpeg" alt="" style="width:100%; height:20%;"><br>
-          <br>
-          <img src="images/panhala-images/pan2.jpeg" alt="" style="width:100%; height:20%;"><br>
-         
-      </div>
-    </div>
-  </div>
-</div>
-<?php
-include_once 'footer.php';
-?>
-
-</body>
-</html>
+    <?php
+    include_once 'footer.php'
+    ?>
