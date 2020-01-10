f14246e6b4e32cca4b925e9384628c609f8d5329
darshan <darshankoli2397@gmail.com>
2020-01-10 09:18:30 +0530

    modified khasbag-maidan.php

--- khasbag-maidan.php
+++ khasbag-maidan.php
@@ -43,6 +43,61 @@ include_once 'header.php'
     </a>
   </div>
 
+  
+  
+  <center><h1 style="color:blue"> Welcome to Panhala Fort </h1></center>
+<div class="">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="box2">
+        <div class="container-fluid">
+
+          <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+              <li data-target="#myCarousel" data-slide-to="1"></li>
+              <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+              <div class="item active">
+                <img src="images/1.png" alt="" style="width:100%; height:50%;">
+              </div>
+              <div class="item">
+                <img src="images/2.jpg" alt="" style="width:100%; height:50%;">
+              </div>
+              <div class="item">
+                <img src="images/3.png" alt="" style="width:100%; height:50%;">
+              </div>
+              <div class="item">
+                <img src="images/4.jpeg" alt="" style="width:100%; height:50%;">
+              </div>
+              <div class="item">
+                <img src="images/5.jpg" alt="" style="width:100%; height:50%;">
+              </div>
+            </div>
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+
+  
+  
+  
   <div class="row">
     <div class="col-md-4">
       <div class="block">
