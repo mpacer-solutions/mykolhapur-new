8061cc5b9d3229f665bbb837ca9a914366afb922
darshan <darshankoli2397@gmail.com>
2020-01-10 09:37:45 +0530

    modified khasbag-maidan.php

--- kaneri-math.php
+++ kaneri-math.php
@@ -4,49 +4,68 @@ $active_kaneri = "active";
 include_once 'header.php'
 ?>
 
+
 <main class="container-fluid">
-  <h1 class="page-title">Welcome To Kaneri Math</h1>
+  <h1 class="page-title">Welcome To Kaneri-Math</h1>
   <div id="myCarousel" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-      <li data-target="#myCarousel" data-slide-to="3"></li>
-      <li data-target="#myCarousel" data-slide-to="4"></li>
-    </ol>
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" >
-      <div class="item active" >
-        <img src="images/kaneri/1.jpg" class="img-responsive" width="100%" height="100%">
+    <div class="">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="box2">
+            <div class="container-fluid">
+
+              <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                  <li data-target="#myCarousel" data-slide-to="1"></li>
+                  <li data-target="#myCarousel" data-slide-to="2"></li>
+                  <li data-target="#myCarousel" data-slide-to="3"></li>
+                  <li data-target="#myCarousel" data-slide-to="4"></li>
+               
+                </ol>
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                  <div class="item active">
+                    <img src="images/kaneri/1.jpg" alt="" style="width:100%; height:100%;">
+                  </div>
+                  <div class="item">
+                    <img src="images/kaneri/2.jpg" alt="" style="width:100%; height:100%;">
+                  </div>
+                  <div class="item">
+                    <img src="images/kaneri/3.jpg" alt="" style="width:100%; height:100%;">
+                  </div>
+                  <div class="item">
+                    <img src="images/kaneri/4.jpg" alt="" style="width:100%; height:100%;">
+                  </div>
+                  <div class="item">
+                    <img src="images/kaneri/5.jpg" alt="" style="width:100%; height:100%;">
+                  </div>
+                </div>
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                  <span class="glyphicon glyphicon-chevron-left"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                  <span class="glyphicon glyphicon-chevron-right"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>
+            </div>
+
+          </div>
+        </div>
       </div>
-      <div class="item" >
-        <img src="images/kaneri/2.jpg" class="img-responsive" width="100%" height="100%">
-      </div>
-      <div class="item" >
-        <img src="images/kaneri/3.jpg" class="img-responsive" width="100%" height="100%">
-      </div>
-      <div class="item" >
-        <img src="images/kaneri/4.jpg" class="img-responsive" width="100%" height="100%">
-      </div>
-      <div class="item" >
-        <img src="images/kaneri/5.jpg" class="img-responsive" width="100%" height="100%">
-      </div>
-    </div><!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>   
-      <span class="sr-only">Next</span>
-    </a>
+    </div>
   </div>
+  
 
   <div class="row">
     <div class="col-md-4">
       <div class="block">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3823.313048424459!2d74.26332281486563!3d16.61105078854821!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc0fee01bd944b9%3A0xa31b60f80821f883!2sSiddhagiri%20Gramjivan%20Sangrahalaya%20-Kaneri%20Math!5e0!3m2!1sen!2sin!4v1578561776675!5m2!1sen!2sin" width="550" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3823.313048424459!2d74.26332281486563!3d16.61105078854821!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc0fee01bd944b9%3A0xa31b60f80821f883!2sSiddhagiri%20Gramjivan%20Sangrahalaya%20-Kaneri%20Math!5e0!3m2!1sen!2sin!4v1578561776675!5m2!1sen!2sin" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
       </div>
     </div>
     <div class="col-md-8">
--- khasbag-maidan.php
+++ khasbag-maidan.php
@@ -8,172 +8,134 @@ include_once 'header.php'
   <h1 class="page-title">Welcome To Khasbag Maidan</h1>
   <div id="myCarousel" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-      <li data-target="#myCarousel" data-slide-to="3"></li>
-      <li data-target="#myCarousel" data-slide-to="4"></li>
-    </ol>
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" >
-      <div class="item active" >
-        <img src="images/khasbagimg/3.jpg" class="img-responsive" width="100%" height="100%">
-      </div>
-      <div class="item" >
-        <img src="images/khasbagimg/8.jpg" class="img-responsive" width="100%" height="100%">
-      </div>
-      <div class="item" >
-        <img src="images/khasbagimg/5.jpg" class="img-responsive" width="100%" height="100%">
-      </div>
-      <div class="item" >
-        <img src="images/khasbagimg/2.jpg" class="img-responsive" width="100%" height="100%">
-      </div>
-      <div class="item" >
-        <img src="images/khasbagimg/1.jpg" class="img-responsive" width="100%" height="100%">
-      </div>
-    </div><!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
+    <div class="">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="box2">
+            <div class="container-fluid">
 
-  
-  
-  <center><h1 style="color:blue"> Welcome to Panhala Fort </h1></center>
-<div class="">
-  <div class="row">
-    <div class="col-md-12">
-      <div class="box2">
-        <div class="container-fluid">
-
-          <div id="myCarousel" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-              <li data-target="#myCarousel" data-slide-to="1"></li>
-              <li data-target="#myCarousel" data-slide-to="2"></li>
-            </ol>
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner">
-              <div class="item active">
-                <img src="images/1.png" alt="" style="width:100%; height:50%;">
-              </div>
-              <div class="item">
-                <img src="images/2.jpg" alt="" style="width:100%; height:50%;">
-              </div>
-              <div class="item">
-                <img src="images/3.png" alt="" style="width:100%; height:50%;">
-              </div>
-              <div class="item">
-                <img src="images/4.jpeg" alt="" style="width:100%; height:50%;">
-              </div>
-              <div class="item">
-                <img src="images/5.jpg" alt="" style="width:100%; height:50%;">
+              <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                  <li data-target="#myCarousel" data-slide-to="1"></li>
+                  <li data-target="#myCarousel" data-slide-to="2"></li>
+                  <li data-target="#myCarousel" data-slide-to="3"></li>
+                  <li data-target="#myCarousel" data-slide-to="4"></li>
+                </ol>
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                  <div class="item active">
+                    <img src="images/khasbagimg/1.jpg" alt="" style="width:100%; height:100%;">
+                  </div>
+                  <div class="item">
+                    <img src="images/khasbagimg/2.jpg" alt="" style="width:100%; height:100%;">
+                  </div>
+                  <div class="item">
+                    <img src="images/khasbagimg/3.jpg" alt="" style="width:100%; height:100%;">
+                  </div>
+                  <div class="item">
+                    <img src="images/khasbagimg/4.jpg" alt="" style="width:100%; height:100%;">
+                  </div>
+                  <div class="item">
+                    <img src="images/khasbagimg/5.jpg" alt="" style="width:100%; height:100%;">
+                  </div>
+                </div>
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                  <span class="glyphicon glyphicon-chevron-left"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                  <span class="glyphicon glyphicon-chevron-right"></span>
+                  <span class="sr-only">Next</span>
+                </a>
               </div>
             </div>
-            <!-- Left and right controls -->
-            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-              <span class="glyphicon glyphicon-chevron-left"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#myCarousel" data-slide="next">
-              <span class="glyphicon glyphicon-chevron-right"></span>
-              <span class="sr-only">Next</span>
-            </a>
+
           </div>
         </div>
-
       </div>
     </div>
   </div>
-</div>
-<br>
+  
 
-  
-  
-  
-  <div class="row">
-    <div class="col-md-4">
-      <div class="block">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3821.683525288535!2d74.22392011435814!3d16.6927128884984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc1000e82e46709%3A0x6f847d026e8b16fc!2sKhasbag%20Maidan!5e0!3m2!1sen!2sin!4v1578552084109!5m2!1sen!2sin" width="500" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+<div class="row">
+      <div class="col-md-4">
+        <div class="block">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3821.683525288535!2d74.22392011435814!3d16.6927128884984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc1000e82e46709%3A0x6f847d026e8b16fc!2sKhasbag%20Maidan!5e0!3m2!1sen!2sin!4v1578552084109!5m2!1sen!2sin" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+        </div>
       </div>
-    </div>
-    <div class="col-md-8">
-      <div class="block">
-        <h2 class="block-title">About Khasbag Maidan</h2>
-        <p>
-          Rajarshi Shahu Kashbag wrestling ground is on west side of Keshavrao Bhosale Drama theatre. 
-          It is used for wrestling competitions surrounding the main west area there is open space for 15 to 2000 spectators. 
-          There are total five gates for entering this wrestling ground. 
-          On the east side of the ground there is a big stage of 35*35 size. In adittion to wrestling competition,
-          big singing programes or shows by cine artist are arranged on this ground.It is an architectural beauty.
-          There is a stage for invited and honourable persons and is covered by pavilion. 
-          Surrounding the ground there are raised steel bases which keep the spectators at appropriate distance and prevent from entering the ground.
-          About 25 to 30 thousand people can sit here. Main entrance is on the east side and has Keshavrao Bhosale drama theatre on its side.
-        </p>
-      </div>
-    </div>
-  </div>
-
-  <h2 class="section-title">Kolhapur city is famous for the game of wrestling in India.</h2>
-
-  <div class="row">
-    <div class="col-sm-6 col-md-4">
-      <div class="thumbnail">
-        <img src="images/khasbagimg/hotel.webp" alt="...">
-        <div class="caption text-center">
-          <h3>Hotels</h3>
-          <p class="content">
-            1.Hotel Royal Treat<br>
-            2.Hotel Madhuri Executive<br>
-            3.Hotel Solanki Guest House<br>
-            4.Hotel Classic Mid Town<br>
-            5.Hotel Silver Oak<br>
+      <div class="col-md-8">
+        <div class="block">
+          <h2 class="block-title">About Khasbag Maidan</h2>
+          <p>
+            Rajarshi Shahu Kashbag wrestling ground is on west side of Keshavrao Bhosale Drama theatre. 
+            It is used for wrestling competitions surrounding the main west area there is open space for 15 to 2000 spectators. 
+            There are total five gates for entering this wrestling ground. 
+            On the east side of the ground there is a big stage of 35*35 size. In adittion to wrestling competition,
+            big singing programes or shows by cine artist are arranged on this ground.It is an architectural beauty.
+            There is a stage for invited and honourable persons and is covered by pavilion. 
+            Surrounding the ground there are raised steel bases which keep the spectators at appropriate distance and prevent from entering the ground.
+            About 25 to 30 thousand people can sit here. Main entrance is on the east side and has Keshavrao Bhosale drama theatre on its side.
           </p>
-          <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
         </div>
       </div>
     </div>
 
-    <div class="col-sm-6 col-md-4">
-      <div class="thumbnail">
-        <img src="images/khasbagimg/shop.jpeg" alt="...">
-        <div class="caption text-center">
-          <h3>Shoping Centers</h3>
-          <p class="content">
-            1.Bhosle Plaza <br>
-            2.Ashok Shopee<br>
-            3.DYP city Mall
-          </p>
-          <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
-        </div>
-      </div>
-    </div>
+    <h2 class="section-title">Kolhapur city is famous for the game of wrestling in India.</h2>
 
-    <div class="col-sm-6 col-md-4">
-      <div class="thumbnail">
-        <img src="images/khasbagimg/6.jpg" alt="...">
-        <div class="caption text-center">
-          <h3>What to see</h3>
-          <p class="content">
-            1.Bhavani Mandap<br>
-            2.Bikhambi Ganesh Temple<br>
-            3.Maidan<br>
-          </p>
-          <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+    <div class="row">
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="images/khasbagimg/hotel.webp" alt="...">
+          <div class="caption text-center">
+            <h3>Hotels</h3>
+            <p class="content">
+              1.Hotel Royal Treat<br>
+              2.Hotel Madhuri Executive<br>
+              3.Hotel Solanki Guest House<br>
+              4.Hotel Classic Mid Town<br>
+              5.Hotel Silver Oak<br>
+            </p>
+            <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+          </div>
         </div>
       </div>
+
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="images/khasbagimg/shop.jpeg" alt="...">
+          <div class="caption text-center">
+            <h3>Shoping Centers</h3>
+            <p class="content">
+              1.Bhosle Plaza <br>
+              2.Ashok Shopee<br>
+              3.DYP city Mall
+            </p>
+            <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="images/khasbagimg/6.jpg" alt="...">
+          <div class="caption text-center">
+            <h3>What to see</h3>
+            <p class="content">
+              1.Bhavani Mandap<br>
+              2.Bikhambi Ganesh Temple<br>
+              3.Maidan<br>
+            </p>
+            <p><a href="#" class="btn btn-primary" role="button">Read more</a></p>
+          </div>
+        </div>
+      </div>
+
     </div>
 
   </div>
-
-</div>
 </main>
 
 <?php
